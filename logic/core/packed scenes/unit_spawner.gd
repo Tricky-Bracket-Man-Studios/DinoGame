fd5c1ae3347cf39eafba9d50b562c30ccf942d37
unknown <matthew92902@gmail.com>
2026-09-09 07:40:09 -0400

    Added a unitspawner, not finished!

---
+++ logic/core/packed scenes/unit_spawner.gd
@@ -0,0 +1,8 @@
+class_name UnitSpawner
+extends Node2D
+
+## Description: The purpose of this script is to have set locations where units
+## spawn and to control the logic behind it.
+## Filename: unit_spawner.gd
+## Author(s): Matthew Perry,
+## Last Updated: 09/09/2026
